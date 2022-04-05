176ae164a372bee9addb3cd54b2e98718cf9328c
lmrosado <laura@rosadoatelier.com>
2022-04-05 18:33:09 -0400

    euclidean distance function, updated callback functions, added some docstrings

--- src/path_planning.py
+++ src/path_planning.py
@@ -9,7 +9,8 @@ import time, os
 from utils import LineTrajectory
 
 class PathPlan(object):
-    """ Listens for goal pose published by RViz and uses it to plan a path from
+    """ 
+    Listens for goal pose published by RViz and uses it to plan a path from
     current car pose.
     """
 
@@ -36,14 +37,39 @@ class PathPlan(object):
         self.map = np.where(self.map < 0, 1, self.map)
         self.map = np.where(self.map > self.occupancy_threshold, 1, 0)
 
-        # how many times is this called?
 
     def odom_cb(self, msg):
-        self.start = msg.pose.pose
+        """
+        Odometry.msg
+            Header header
+            string child_frame_id
+            geometry_msgs/PoseWithCovariance pose
+            geometry_msgs/TwistWithCovariance twist
+
+        PoseWithCovariance.msg
+            Pose pose
+            # (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)
+            float64[36] covariance
+        """
+        self.start = msg.pose.pose[:3] # [x, y, z]
 
 
     def goal_cb(self, msg):
-        self.goal = msg
+        """
+        PoseStamped.msg
+            Header header
+            Pose pose
+
+        Pose.msg
+            Point position
+            Quaternion orientation
+
+        Point.msg
+            float64 x
+            float64 y
+            float64 z
+        """
+        self.goal = [msg.pose.Point.x, msg.pose.Point.y, msg.pose.Point.z] # [x, y, z]
 
 
     def convert_xy_to_uv(self, pose):
@@ -54,6 +80,31 @@ class PathPlan(object):
 
         # use to convert start and end positions into (u, v) frame for plan_path()
         pass 
+
+    def get_euclidean_distance(self, start_point, end_point):
+        """
+        Calculates the Euclidean distance between two points.
+        Intended for use as a heuristic.
+        
+        Inputs:
+            start_point: position array
+            end_point: position array
+
+        Outputs:
+            distance (float)
+        """
+
+        if len(start_point) != len(end_point)
+            rospy.loginfo("Unable to compute Euclidean distance.")
+            return
+
+        return np.sqrt(np.sum(np.square(end_point-start_point)))
+
+    def get_neighbors(self, point):
+        """
+        Finds all neighbors to a given point.
+        """
+        pass
 
     def astar_search(self, start_point, end_point, map):
         pass
